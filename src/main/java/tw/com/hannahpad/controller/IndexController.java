7e4bc7f908d6596f7c2db38f88a1f66baa054c3d
chrislin <lemon0417@gmail.com>
2020-03-23 17:48:52 +0800

    修正頁數計算

--- src/main/java/tw/com/hannahpad/controller/IndexController.java
+++ src/main/java/tw/com/hannahpad/controller/IndexController.java
@@ -7,12 +7,14 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import tw.com.hannahpad.dto.DetailDto;
 import tw.com.hannahpad.service.SevenElevenService;
 import tw.com.hannahpad.service.SlackService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
+import java.util.List;
 import java.util.Set;
 
 @Validated
@@ -34,7 +36,9 @@ public class IndexController {
     public String getPreview(@RequestParam Set<@Valid @Pattern(regexp = "^[A-Z][0-9]{11}$", message = "格式不符") String> ids,
                              ModelMap map, HttpServletRequest request) {
         slack.sendMessage(ids, request);
-        map.addAttribute("list", service.entry(ids));
+        List<DetailDto> list = service.entry(ids);
+        map.addAttribute("list", list);
+        map.addAttribute("totalPages", Math.ceil(list.size() / 4.0));
         return "/preview";
     }
 
