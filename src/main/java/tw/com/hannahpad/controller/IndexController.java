4d7fee0d60631f82e2353e21d8a1dc9cbb54a762
chrislin <lemon0417@gmail.com>
2020-03-28 13:08:35 +0800

    新增可選每頁筆數

--- src/main/java/tw/com/hannahpad/controller/IndexController.java
+++ src/main/java/tw/com/hannahpad/controller/IndexController.java
@@ -1,5 +1,6 @@
 package tw.com.hannahpad.controller;
 
+import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -34,11 +35,13 @@ public class IndexController {
 
     @PostMapping(value = "/preview")
     public String getPreview(@RequestParam Set<@Valid @Pattern(regexp = "^[A-Z][0-9]{11}$", message = "格式不符") String> ids,
+                             @RequestParam @Valid @Range(min = 1, max = 4, message = "數量錯誤") Integer perPage,
                              ModelMap map, HttpServletRequest request) {
         slack.sendMessage(ids, request);
         List<DetailDto> list = service.entry(ids);
         map.addAttribute("list", list);
-        map.addAttribute("totalPages", Math.ceil(list.size() / 4.0));
+        map.addAttribute("perPage", perPage);
+        map.addAttribute("totalPages", Math.ceil(list.size() * 1.0 / perPage));
         return "/preview";
     }
 
